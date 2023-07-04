d7330ac9d181d88685e7247fef10bcd6e07ba8ab
Dove-White <dugbazahredeemer@gmail.com>
2023-07-04 14:51:33 +0000

    at main section-two

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '414px',
+      'md': '908px',
+      'xl': '1200px',
+    },
+  },
+  plugins: [],
+}
+
